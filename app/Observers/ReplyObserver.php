cc12081da11555438d5c750b5c555c2cd5cfac7a
vagrant <vagrant@homestead>
2018-02-19 02:58:13 +0000

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -19,9 +19,14 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         $topic = $reply->topic;
-        $topic->increment('reply_count', 1);
+        $topic->increment('reply_count', 1); // +1
 
         // 通知作者话题被回复了
         $topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1); // -1
     }
 }
